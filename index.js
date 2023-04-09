defaf733968df6c75effbf56978ebb8120447cbf
jannat456nur <jannatjenny22@gmail.com>
2023-04-09 13:01:28 +0600

    book delete added and review delete added

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     const bookDataCollection = client.db('mylibrary').collection('bookData')
     const feedbackCollection = client.db('mylibrary').collection('feedback')
     const usersCollection = client.db('mylibrary').collection('users')
+    const coursesCollection = client.db('mylibrary').collection('course')
     //get book data
     app.get('/bookDataCollection', async (req, res) => {
       const query = {}
@@ -69,23 +70,54 @@ async function run() {
       res.json(result)
     })
 
-    // app.get("/doctors/:email", async (req, res) => {
-    //   const {email} = req.params;
-    //   const cursor = doctorsCollection.find({email});
-    //   const doctor = await cursor.toArray();
-    //   res.json(doctor);
-    // });
-    // // User sending to db
-
-    // // User upsert function
-    // app.put("/users", async (req, res) => {
-    //   const user = req.body;
-    //   const filter = { email: user.email };
-    //   const options = { upsert: true }; //used for upsert = uodate if exist else post
-    //   const updateDoc = { $set: user };
-    //   const result = await usersCollection.updateOne(filter,updateDoc,options);
-    //   res.json(result);
-    // });
+    app.get('/users', async (req, res) => {
+      const query = {}
+      const cursor = usersCollection.find(query)
+      const feedback = await cursor.toArray()
+      res.json(feedback)
+    })
+    // // Delete COURSE API
+    // app.delete('/courses/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const query = { _id: ObjectId(id) }
+    //   const result = await coursesCollection.deleteOne(query)
+    //   res.json(result)
+    // })
+    app.put('/users/admin/:id', async (req, res) => {
+      const user = req.body
+      console.log(user)
+      const filter = { _id: ObjectId(req.params.id) } //work like query
+      const options = { upsert: true }
+      const updateDoc = { $set: { role: 'admin' } }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      console.log(result)
+      res.json(result)
+    })
+    // Delete FEEDBACK API
+    app.delete('/feedback/:id', async (req, res) => {
+      const id = req.params.id
+      console.log(id, 'deleted')
+      const query = { _id: new ObjectId(id) }
+      const result = await feedbackCollection.deleteOne(query)
+      res.json(result)
+    })
+    // Delete BOOK API
+    app.delete('/bookDataCollection/:id', async (req, res) => {
+      const id = req.params.id
+      console.log(id, 'deleted')
+      const query = { _id: new ObjectId(id) }
+      const result = await bookDataCollection.deleteOne(query)
+      res.json(result)
+    })
+    // // Update COURSE API
+    // app.put('/users', async (req, res) => {
+    //   const user = req.body
+    //   console.log('put', user)
+    //   const filter = { email: user.email }
+    //   const updateDoc = { $set: { role: 'admin' } }
+    //   const result = await usersCollection.updateOne(filter, updateDoc)
+    //   res.json(result)
+    // })
   } finally {
     //   await client.close();
   }
